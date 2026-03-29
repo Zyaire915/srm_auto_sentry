70df57b8e0ca98e113cc417166c29cfca2513ea9
“Zyaire915” <roamcosmos7@gmail.com>
2026-03-29 10:22:31 +0800

    111

--- src/pb_rmsimulation/src/rm_nav_bringup/launch/bringup_real.launch.py
+++ src/pb_rmsimulation/src/rm_nav_bringup/launch/bringup_real.launch.py
@@ -177,13 +177,13 @@ def generate_launch_description():
         parameters=[{
             'target_frame': 'livox_frame',
             'transform_tolerance': 0.01,
-            'min_height': -0.1,  ##改
+            'min_height': -0.2,  ##改
             'max_height': 0.1,
             'angle_min': -3.14159,  # -M_PI/2
             'angle_max': 3.14159,   # M_PI/2
             'angle_increment': 0.0043,  # M_PI/360.0
             'scan_time': 0.3333,
-            'range_min': 0.45,
+            'range_min': 0.3,
             'range_max': 10.0,
             'use_inf': True,
             'inf_epsilon': 1.0
